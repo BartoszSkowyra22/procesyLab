18ef91b4e4507795b9d2e7939c1be6cb26d10d0c
BartoszSkowyra22 <skowyrab@gmail.com>
2024-10-10 19:32:05 +0200

    Hello world 7,8,9

--- src/main/java/com/pollub/Main.java
+++ src/main/java/com/pollub/Main.java
@@ -7,6 +7,9 @@ public class Main {
         System.out.println("Hello world!3");
         System.out.println("Hello world!4");
         System.out.println("Hello world!6");
+        System.out.println("Hello world!7");
+        System.out.println("Hello world!8");
+        System.out.println("Hello world!9");
         System.out.println("Hello world from develop!");
     }
 }
